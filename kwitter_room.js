07ea28452917f6176dcd3e36e22cd8be4c22ed6b
Alumnobyjus1 <104869929+Alumnobyjus1@users.noreply.github.com>
2022-10-29 12:50:43 -0500

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,6 +1,16 @@
 
 //AÑADE TUS ENLACES DE FIREBASE
-
+var firebaseConfig = {
+      apiKey: "AIzaSyAiTtof9ThUBhwF45zy4t9WxplREIXR67c",
+      authDomain: "croco-chat-b3367.firebaseapp.com",
+      projectId: "croco-chat-b3367",
+      databaseURL=https:-"//croco-chat-b3367-default-rtdb.firebaseio.com",
+      storageBucket: "croco-chat-b3367.appspot.com",
+      messagingSenderId: "791401896416",
+      appId: "1:791401896416:web:d733eb0983df3918b2b385",
+      measurementId: "G-RW874P16XJ"
+    };
+    firebase.initializeApp(firebaseConfig);
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
       //Inicio del código
